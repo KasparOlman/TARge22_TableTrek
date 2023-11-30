158b88961fadeef05696786cc31e06337d7ffb03
Kaspar <kaspar.olman97gmail.com>
2023-11-30 16:41:27 +0200

    As an app developer I want to get full details of a given restaurants to show in my app (#186547906)

--- index.js
+++ index.js
@@ -4,8 +4,53 @@ const port = 8080;
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
-app.get("/games", (req, res) => {
-  res.send(["Witcher 3", "Cyberpunk 2077"]);
+const restaurants = [
+  {
+    id: 0,
+    name: "Select a restaurant",
+    price: "?",
+  },
+  {
+    id: 1,
+    name: "Art Priori",
+    price: "$$",
+  },
+  {
+    id: 2,
+    name: "La Cuccina",
+    price: "$$$",
+  },
+  {
+    id: 3,
+    name: "Fotografiska",
+    price: "$$",
+  },
+  {
+    id: 4,
+    name: "Lee",
+    price: "$$$",
+  },
+  {
+    id: 5,
+    name: "Noa",
+    price: "$$$",
+  },
+  {
+    id: 6,
+    name: "NCH",
+    price: "$$$$",
+  },
+];
+
+app.get("/restaurants", (req, res) => {
+  res.send(restaurants);
+});
+
+app.get("/restaurants/:id", (req, res) => {
+  if (typeof restaurants[req.params.id - 1] === "undefined") {
+    return res.status(404).send({ error: "Restaurant not found." });
+  }
+  res.send(restaurants[req.params.id - 1]);
 });
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
