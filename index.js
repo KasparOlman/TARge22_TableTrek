2bad79c1edca730988f823911a519733c7e7b4dc
Kaspar <kaspar.olman97gmail.com>
2023-12-07 15:06:07 +0200

    added delete fucntion

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const restaurants = [
     price: "$$$$",
   },
   {
-    id: 6,
+    id: 7,
     name: "Barbarea",
     price: "$$",
   },
@@ -58,5 +58,37 @@ app.get("/restaurants/:id", (req, res) => {
   res.send(restaurants[req.params.id - 1]);
 });
 
+app.post("/restaurants", (req, res) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).send({ error: "Name and price are required." });
+  }
+
+  const newRestaurant = {
+    id: restaurants.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  restaurants.push(newRestaurant);
+
+  res.status(201).send(newRestaurant);
+});
+
+app.delete("/restaurants/:id", (req, res) => {
+  if (typeof restaurants[req.params.id - 1] === "undefined") {
+    return res.status(404).send({ error: "Restaurant not found." });
+  }
+
+  restaurants.splice(req.params.id - 1, 1);
+
+  res.status(204).send({ error: "Restaurant deleted." });
+});
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => console.log(`API up at http://localhost:${port}`));
+
+function getBaseUri(req) {
+  return req.connection && req.connection.encrypted
+    ? "https"
+    : "http" + `://${req.headers.host}`;
+}
